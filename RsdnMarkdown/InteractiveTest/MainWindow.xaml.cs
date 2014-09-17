38a9b57d550f533b5a1f00f9103a61bf8d2d4313
VladD2 <vc@rsdn.ru>
2014-09-17 18:29:11 +0400

    Implement SplitQuotations().

--- RsdnMarkdown/InteractiveTest/MainWindow.xaml.cs
+++ RsdnMarkdown/InteractiveTest/MainWindow.xaml.cs
@@ -1,5 +1,10 @@
-﻿using System;
+﻿using Nitra;
+
+using RsdnMarkdown;
+
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -27,7 +32,21 @@ namespace InteractiveTest
 
     private void _text_TextChanged(object sender, TextChangedEventArgs e)
     {
-      var html = Properties.Resources.HtmlTemplate.Replace("{content}", _text.Text); ;
+      var reader = new StringReader(_text.Text);
+      var blocks = Utils.SplitQuotations(reader);
+      var sb = new StringBuilder();
+      sb.AppendLine("<table border=\"1\">");
+      foreach (var block in blocks)
+      {
+        sb.AppendLine("<tr><td><b>" + block.Key + "</b></td><td>" + block.Value.Replace("\n", "<br>\n") + "</td></tr>");
+      }
+      sb.AppendLine("</table>");
+
+      //var parserHost  = new ParserHost();
+      //var source      = new SingleLineSourceSnapshot(_text.Text);
+      //var parseResult = Paragraph.Start(source, parserHost);
+      //var ast         = ParagraphAst.Start.Create(parseResult);
+      var html = Properties.Resources.HtmlTemplate.Replace("{content}", sb.ToString()); ;
       _htmlViewer.NavigateToString(html);
     }
   }
