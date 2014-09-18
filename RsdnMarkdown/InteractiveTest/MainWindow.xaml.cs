c831385ed4c740a6f8849bee481e66b0e0509f16
VladD2 <vc@rsdn.ru>
2014-09-18 18:09:03 +0400

    Fix bugs. Add the _openInBrowser button.

--- RsdnMarkdown/InteractiveTest/MainWindow.xaml.cs
+++ RsdnMarkdown/InteractiveTest/MainWindow.xaml.cs
@@ -3,6 +3,7 @@
 using RsdnMarkdown;
 
 using System;
+using System.Diagnostics;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -18,6 +19,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using Path = System.IO.Path;
+
 namespace InteractiveTest
 {
   /// <summary>
@@ -32,22 +35,34 @@ namespace InteractiveTest
 
     private void _text_TextChanged(object sender, TextChangedEventArgs e)
     {
+      var html = GetHtml();
+      _htmlViewer.NavigateToString(html);
+    }
+
+    private string GetHtml()
+    {
       var reader = new StringReader(_text.Text);
       var blocks = Utils.SplitQuotations(reader);
       var sb = new StringBuilder();
-      sb.AppendLine("<table border=\"1\">");
+      sb.AppendLine("<table border=\"1\" width=\"100%\">");
       foreach (var block in blocks)
       {
-        sb.AppendLine("<tr><td><b>" + block.Key + "</b></td><td>" + block.Value.Replace("\n", "<br>\n") + "</td></tr>");
+        var blockContent = string.Join("<br>\r\n", Utils.ProcessQuotationBlock(block.Value));
+        sb.AppendLine("<tr><td><b>" + block.Key + "</b></td><td>" + blockContent + "</td></tr>");
       }
       sb.AppendLine("</table>");
 
-      //var parserHost  = new ParserHost();
-      //var source      = new SingleLineSourceSnapshot(_text.Text);
-      //var parseResult = Paragraph.Start(source, parserHost);
-      //var ast         = ParagraphAst.Start.Create(parseResult);
       var html = Properties.Resources.HtmlTemplate.Replace("{content}", sb.ToString()); ;
-      _htmlViewer.NavigateToString(html);
+      return html;
+    }
+
+    private void _openInBrowser_Click(object sender, RoutedEventArgs e)
+    {
+      var html = GetHtml();
+      var fileName = Path.GetTempFileName();
+      fileName = Path.ChangeExtension(fileName, "html");
+      File.WriteAllText(fileName, html, Encoding.UTF8);
+      Process.Start(fileName);
     }
   }
 }
