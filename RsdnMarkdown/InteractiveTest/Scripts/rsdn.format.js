6cfaf95489538bf8a397a94dd93ecfcbf536303d
andrewvk <avk@rsdn.ru>
2014-09-27 02:25:28 +0400

    TS support

--- RsdnMarkdown/InteractiveTest/Scripts/rsdn.format.js
+++ RsdnMarkdown/InteractiveTest/Scripts/rsdn.format.js
@@ -1,16 +1,32 @@
-﻿var RsdnFormat;
+﻿/// <reference path="typings/jquery/jquery.d.ts"/>
+
+var RsdnFormat;
 (function (RsdnFormat) {
     $(function () {
-        $('div img').parent().addClass('image');
-        $('.foldable .block-name').click(function (evt) {
+        var format = $('.format');
+
+        // Add class for proper image handles styling
+        format.find('div img').parent().addClass('image');
+        format.find('div blockquote').parent().addClass('quote');
+        format.find('div .code').parent().addClass('code-box');
+
+        // Foldable blocks
+        format.find('.foldable .block-name').click(function (evt) {
             evt.preventDefault();
             var handle = $(evt.delegateTarget);
             handle.next().toggle('fast');
             handle.parent().toggleClass('folded');
         });
-        $('.code').each(function (idx, elem) {
+
+        // Code syntax highlight
+        format.find('.code').each(function (idx, elem) {
             hljs.highlightBlock(elem);
         });
+
+        // Table zebra
+        var rows = format.find('table tr');
+        rows.filter(':odd').addClass('odd');
+        rows.filter(':even').addClass('even');
     });
 })(RsdnFormat || (RsdnFormat = {}));
 //# sourceMappingURL=rsdn.format.js.map
